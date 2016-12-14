6eaff1434cae3bfa85e1b0e6eb1c29e1af41f7d5
Nick Hilman <Nhilman1012@gmail.com>
2016-12-14 15:09:43 -0500

    update

--- public/js/ajax/buy.js
+++ public/js/ajax/buy.js
@@ -4,24 +4,24 @@ $(function (){
 
     $.ajax({
         type: 'GET',
-        url: 'http://35.161.233.165:9000/api_pending/spots_pending',
+        url: 'http://35.165.226.154:3030/api/iteam',
         success: function(spotz) {
             $.each(spotz, function(i, spot) {
                 $spotz.append( '<div class="row parking-listings">' +
                                     '<div class="col-lg-2 col-xs-12 listing-title">' +
-                                        '<h4>' + spot.firstName + " " + spot.lastName + '</h4>' + 
+                                        '<h4>' + spot.first + " " + spot.last + '</h4>' + 
                                     '</div>'+
                                     '<div class="col-lg-2 col-xs-12 listing-title">' +
-                                        '<h4>' + spot.type + '</h4>' + 
+                                        '<h4>' + spot.email + '</h4>' + 
                                     '</div>'+
                                     '<div class="col-lg-2 col-xs-12 listing-title">' +
-                                        '<h4>' + spot.adress + '</h4>' + 
-                                    '</div>'+
-                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
-                                        '<h4>' + spot.phoneNumber + '</h4>' + 
+                                        '<h4>' + spot.phone + '</h4>' + 
                                     '</div>'+
                                     '<div class="col-lg-2 col-xs-12 listing-title">' +
                                         '<h4>' + spot.title + '</h4>' + 
+                                    '</div>'+
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
+                                        '<h4>' + spot.price + '</h4>' + 
                                     '</div>'+
                                 '</div>' );
             });
