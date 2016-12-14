74c04a4d13b473329645b37e5234654cb296a054
Nick Hilman <Nhilman1012@gmail.com>
2016-12-14 10:09:54 -0500

    added .js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 //Models
-var Spot = require('../model/spot');
+var Spot = require('../model/spot.js');
 
 // Routes
 Spot.methods(['get', 'put', 'post', 'delete']);
